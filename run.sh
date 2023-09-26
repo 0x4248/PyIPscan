e1592ab37ce358815942be9c4962434c8a2cc148
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-09-26 19:00:27 +0100

    Move code to src
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Python IP Scanner
+# Picks random IP addresses and scans them for open HTTP and HTTPS ports.
+# Github: https://www.github.com/lewisevans2007/pyipscan
+# License: GNU General Public License v3.0
+# By: Lewis Evans
+
+python="python3"
+
+$python src/main.py
